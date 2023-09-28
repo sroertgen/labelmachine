6e55cc4df55b8fa6ab7b5262626a47ad0c4211d1
@s.roertgen <sroertgen@gmail.com>
2023-09-28 21:13:23 +0200

    add more relays

--- src/lib/ndk.js
+++ src/lib/ndk.js
@@ -1,9 +1,12 @@
 import { writable } from 'svelte/store';
-import NDK, { NDKNip07Signer } from '@nostr-dev-kit/ndk';
+import NDK from '@nostr-dev-kit/ndk';
 
 const ndk = new NDK({
   explicitRelayUrls: [
+    'wss://nostr.btc-library.com',
     'wss://purplepag.es',
+    'wss://relay.nostr.band',
+    'wss://nos.lol',
     'wss://relay.snort.social',
     'wss://relay.damus.io'
   ]
