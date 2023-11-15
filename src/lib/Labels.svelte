c8a4bc7eda1f20ed62e4a822269598cf2121922a
@s.roertgen <sroertgen@gmail.com>
2023-11-15 22:01:23 +0100

    Improve display of assgined labels a bit

--- src/lib/Labels.svelte
+++ src/lib/Labels.svelte
@@ -8,16 +8,24 @@
 			{#if label.selectedType == 'Topic'}
 				<span>Topic:</span>
 				{#each label.topics.split(',') as topic}
-					<span class="mx-2 badge badge-warning">{topic}</span>
+					<span class="mx-2 badge badge-warning h-max">{topic}</span>
 				{/each}
 			{:else if label.selectedType == 'Pubkey'}
-				Pubkey: <div class="mx-2 badge badge-warning">{label.pubkey}</div>
+				Pubkey: <div class="mx-2 badge badge-warning h-max">{label.pubkey}</div>
 			{:else if label.selectedType == 'Custom Label'}
-				<span
-					>Namespace: <div class="mx-2 badge badge-warning">{label.labelNamespace}</div></span
-				>
-				<span>Label:</span>
-				<div class="mx-2 badge badge-warning">{label.label}</div>
+				<p>
+					<span
+						>Namespace: <div class="mx-2 badge badge-warning h-max">
+							{label.labelNamespace}
+						</div></span
+					>
+				</p>
+				<p>
+					<span
+						>Label:
+						<div class="mx-2 badge badge-warning h-max">{label.label}</div>
+					</span>
+				</p>
 			{/if}
 		</div>
 	{/each}
